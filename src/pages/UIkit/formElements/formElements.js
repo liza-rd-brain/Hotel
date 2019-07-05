6f6a61a450d7d987f0e0fccf423366b8589330a7
liza <lisaminakova@gmail.com>
2019-07-06 01:30:47 +0300

    Доработала верхнуюю часть footer-a. Разобралась с svg стрелкой (цвет, тень).Пока считаю неплохим вариантом сложные и меняющиеся в окрасе картинки делать через svg.

--- src/pages/UIkit/formElements/formElements.js
+++ src/pages/UIkit/formElements/formElements.js
@@ -4,6 +4,7 @@ import "normalize.css";
 import "../../../components/textField/textField.scss";
 import "../../../components/select-item/select-item.scss"
 import "../../../components/logo-icon/logo-icon.scss"
+import "../../../components/arrow/arrow.scss"
 /* import "../../../components/button/button.scss" */
 
 
