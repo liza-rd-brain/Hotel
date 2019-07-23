da127cc58f8e21c03687b3e902b2c74779baa76b
liza <lisaminakova@gmail.com>
2019-07-23 20:54:19 +0300

    Продолжена стилизация комплексного дропдауна. Встает проблема в корректировке функционала item-quantity-dropdown

--- src/components/select-item/select-item.js
+++ src/components/select-item/select-item.js
@@ -1,32 +1,67 @@
-import 'item-quantity-dropdown/lib/item-quantity-dropdown.min.js';
-import 'item-quantity-dropdown/lib/item-quantity-dropdown.min.css';
+import "item-quantity-dropdown/lib/item-quantity-dropdown.min.js";
+import "item-quantity-dropdown/lib/item-quantity-dropdown.min.css";
 import "./select-item.scss";
 import "../select-menu/select-menu";
 // import easydropdown from 'easydropdown';
 
+// $(".select-item__wrap").click(function(event) {
+//   var select = $(event.target).children(".select-item__select");
+//   select.toggleClass("select-item__select_open");
+//   /*  $(event.target).toggleClass("select-item__wrap_open"); */
+//   /* $(event.target).children(".select-item__options").addClass("select-item__options_visible"); */
 
-$(".select-item__wrap").click(function(event) {
-  var select = $(event.target).children(".select-item__select");
-  select.toggleClass("select-item__select_open");
- /*  $(event.target).toggleClass("select-item__wrap_open"); */
-  /* $(event.target).children(".select-item__options").addClass("select-item__options_visible"); */
+//   var options = $(event.target).children(".select-item__options");
+//   options.toggleClass("select-item__options_visible");
+// });
 
-  var options = $(event.target).children(".select-item__options");
-  options.toggleClass("select-item__options_visible");
-});
-
-
-
-$(".select-item__wrap").on('click', function() {
-  alert($(this).attr('class'));
-});
-console.log("test");
-console.log($);
+// $(".select-item__wrap").on("click", function() {
+//   alert($(this).attr("class"));
+// });
+// console.log("test");
+// console.log($);
 
 // easydropdown.all()
 // const edd = easydropdown('#my-select');
+const GUEST = {
+  maxItems: Infinity,
+  minItems: 1,
+  selectionText: "гость",
+  textPlural: "гостя",
+  controls: {
+    position: "right",
+    displayCls: "iqdropdown-content",
+    controlsCls: "iqdropdown-item-controls",
+    counterCls: "counter"
+  },
+  items: {
+    /* test:"test" */
+  },
+  onChange: (id, count, totalItems) => {},
+  beforeDecrement: () => true,
+  beforeIncrement: () => true
+};
 
+/* var textPluralDef = (items>1 && items<5)?"гостя":"гостей" */
 
-$(document).ready(() => {
-  $('.iqdropdown').iqDropdown({ });
+$().ready(() => {
+  $(".iqdropdown").iqDropdown(GUEST);
+  /*   $(".iqdropdown").iqDropdown({}); */
+  //дефолтный текст
+  $(".iqdropdown-selection").text("Сколько гостей");
+
+  /* $(`.button-decrement.button-increment`);
+   */
+
+  /*  selectionText("item"); */
+});
+
+$(`button-increment`).click(() => {
+  debugger;
+  console.log("test");
+});
+
+  
+$(`button-increment`).on('click', function(){
+  debugger;
+  console.log("test");
 });
--- src/components/select-menu/select-menu.js
+++ src/components/select-menu/select-menu.js
@@ -1,3 +1,16 @@
 import 'item-quantity-dropdown/lib/item-quantity-dropdown.min.js';
 import 'item-quantity-dropdown/lib/item-quantity-dropdown.min.css';
 import "./select-menu.scss";
+
+
+
+  // easydropdown.all()
+  // const edd = easydropdown('#my-select');
+ 
+  
+  /* var textPluralDef = (items>1 && items<5)?"гостя":"гостей" */
+  
+  $(`button-increment`).on('click', function(){
+    debugger;
+    console.log("test");
+  });
