a9fc94b5397e251ef7a1b521a5d30b82e91a441a
liza <lisaminakova@gmail.com>
2019-07-26 12:57:45 +0300

    Начала стилизовать календарь. Столкнуласьс проблемой: не могу понять, как компилироваться pug файл на лету в html код, чтобы иметь возможность вставить в разметку

--- src/components/select-date/select-date.js
+++ src/components/select-date/select-date.js
@@ -1,12 +1,42 @@
 import "./select-date.scss";
 import "jquery-datetimepicker/build/jquery.datetimepicker.min.css";
 import "jquery-datetimepicker/build/jquery.datetimepicker.full.min.js";
+import "../arrow/arrow";
 
 jQuery("#datetimepicker").datetimepicker();
-jQuery.datetimepicker.setLocale('ru');
+jQuery.datetimepicker.setLocale("ru");
 jQuery("#datetimepicker").datetimepicker({
-  yearpicker: false,
   timepicker: false,
   format: "d.m.Y",
-  scrollMonth:false
+  dayOfWeekStart: 1,
+  todayButton: false,
+  scrollMonth: false,
+  closeOnWithoutClick: false,
+  closeOnDateSelect: 0
 });
+// $(".xdsoft_datetimepicker").addClass("my_datetimepicker");
+// $(".xdsoft_label").addClass("my_datetimepicker");
+
+//всем элементам календаря дала доп класс .my_datetimepicker для быстрой стилизации
+$('[class^="xdsoft"]').addClass("my_datetimepicker");
+/* $('.xdsoft_date').addClass("my_datetimepicker"); */
+/* $('[class^="xdsoft_datetimepicker"]').addClass("my_datetimepicker"); */
+
+$("#input").datetimepicker();
+$("button.somebutton").on("click", function() {
+  $("#input").datetimepicker("show");
+});
+$(".xdsoft_next").append(
+  '<svg class="arrow-icon arrow-icon_colored" width="26" height="26" fill: url(#paint0_linear)/>'
+);
+
+
+
+$.get('arrow.pug').then(function(doc) {
+  var test_html = pug.compile(doc)();
+  $($(".test_2")).append(test_html);
+});
+
+
+/* let testElem = myElem;
+$(".test_2").append(test_elem); */
