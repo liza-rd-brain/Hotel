332ff805c598fd02f6dbd4b5440360450e31bab4
liza <lisaminakova@gmail.com>
2019-08-16 22:16:02 +0300

    Добила стилизацию и функционал select-элемента с выбором гостей. Плюс немного внесла коррективы в блок кнопки -она может принимать доп класс

--- src/components/select-guest/select-guest.js
+++ src/components/select-guest/select-guest.js
@@ -1,7 +1,7 @@
 import "item-quantity-dropdown/lib/item-quantity-dropdown.min.js";
 import "item-quantity-dropdown/lib/item-quantity-dropdown.min.css";
 import "./select-guest.scss";
-import "../select-menu/select-menu";
+// import "../select-menu/select-menu";
 // import easydropdown from 'easydropdown';
 
 // $(".select-item__wrap").click(function(event) {
@@ -22,8 +22,11 @@ import "../select-menu/select-menu";
 
 // easydropdown.all()
 // const edd = easydropdown('#my-select');
+
+//максимум - 10 гостей
+
 const GUEST = {
-  maxItems: Infinity,
+  maxItems: 10,
   minItems: 1,
   selectionText: "гость",
   textPlural: "гостя",
@@ -41,21 +44,92 @@ const GUEST = {
   beforeIncrement: () => true
 };
 
-/* var textPluralDef = (items>1 && items<5)?"гостя":"гостей" */
-
 $().ready(() => {
   $(".iqdropdown").iqDropdown(GUEST);
- 
   $(".iqdropdown-selection").text("Сколько гостей");
+  checkText();
+  checkButtons();
 });
 
-$(`button-increment`).click(() => {
-  debugger;
-  console.log("test");
+let checkText = () => {
+  //можно ли вынести как константы?!
+  let btnInc = $(document).find(".button-increment");
+  let btnDec = $(document).find(".button-decrement");
+
+  btnInc.on("click", function() {
+    changeText();
+    checkButtons();
+  });
+
+  btnDec.on("click", function() {
+    changeText();
+    checkButtons();
+  });
+};
+
+let changeText = () => {
+  //функция будет вызываться при нажатии на кнопки плюс или минус
+  //проверяем первый символ, если он больше 4 - меняем "гостя" на "гостей" и обратно
+
+  let $text = $(document)
+    .find("p.iqdropdown__text")
+    .text();
+  if ($text.slice(0, 2) > 4) {
+    $(document)
+      .find("p.iqdropdown__text")
+      .text(`${$text.slice(0, 2)} гостей`);
+  }
+};
+
+let checkButtons = () => {
+  for (let i = 1; i < 4; i++) {
+    let counter = $(`div[data-id='item${i}']  .counter`).text();
+    console.log(counter);
+    $(`div[data-id='item${i}'] .button-increment`).addClass("btn_visible");
+
+    if (counter > 0 && counter < 9) {
+      $(`div[data-id='item${i}']  .button-decrement`).addClass("btn_visible");
+    } else if (counter > 9) {
+      $(`div[data-id='item${i}']  .button-increment`).removeClass(
+        "btn_visible"
+      );
+    } else if (counter < 1) {
+      $(`div[data-id='item${i}']  .button-decrement`).removeClass(
+        "btn_visible"
+      );
+    }
+  }
+
+  //   for (let i = 0; i < counter.length; i++) {
+  //     console.log(counter[i]);
+  // /*      if (counter[i].text().slice(0) > 0) {
+  //       btnDec[i].addClass("btn_visible");
+  //       console.log(btnDec[i]);
+  //     } */
+  //     break;
+  //   }
+};
+
+//работающий функционал кнопки очистить!!!!
+$(".button_clear").on("click", () => {
+  $(".iqdropdown")
+    .find(".iqdropdown-item-controls")
+    .addClass("my_iqdropdown")
+    .remove();
+  $(".iqdropdown").iqDropdown(GUEST);
+
+  $(".iqdropdown-selection").text("Сколько гостей");
+  checkText();
+  checkButtons();
 });
 
-  
-$(`button-increment`).on('click', function(){
-  debugger;
-  console.log("test");
+$(".button_accept").on("click", () => {
+  $(".iqdropdown__select .my_iqdropdown").addClass("menu-open");
+  console.log("добавлен");
 });
+
+//всем элементам select-menu дала доп класс  "my_iqdropdown"
+$('[class^="iqdropdown"]').addClass("my_iqdropdown");
+$(".my_iqdropdown")
+  .find($(".counter"))
+  .addClass("my_iqdropdown");
--- src/components/select-menu/select-menu.js
+++ src/components/select-menu/select-menu.js
@@ -1,22 +1,27 @@
-import 'item-quantity-dropdown/lib/item-quantity-dropdown.min.js';
-import 'item-quantity-dropdown/lib/item-quantity-dropdown.min.css';
+import "item-quantity-dropdown/lib/item-quantity-dropdown.min.js";
+import "item-quantity-dropdown/lib/item-quantity-dropdown.min.css";
 import "./select-menu.scss";
 
+// 1.сделать рабочими кнопки -применить, очистить
+// 2. подкрутить синтаксис -гост-ь(я,ей)
+// 3. цвет кнопок: если есть куда менять (+ и -)  они не размытые
+
+// easydropdown.all()
+// const edd = easydropdown('#my-select');
+
+/* var textPluralDef = (items>1 && items<5)?"гостя":"гостей" */
+
+$(`button-increment`).click(() => {
+  debugger;
+  console.log("test");
+});
+
+$(`button-increment`).on("click", function() {
+  debugger;
+  console.log("test");
+});
+
+//всем элементам select-menu дала доп класс  "my_iqdropdown"
+$('[class^="iqdropdown"]').addClass("my_iqdropdown");
 
 
-  // easydropdown.all()
-  // const edd = easydropdown('#my-select');
- 
-  
-  /* var textPluralDef = (items>1 && items<5)?"гостя":"гостей" */
-
-  $(`button-increment`).click(() => {
-    debugger;
-    console.log("test");
-  });
-  
-    
-  $(`button-increment`).on('click', function(){
-    debugger;
-    console.log("test");
-  });
