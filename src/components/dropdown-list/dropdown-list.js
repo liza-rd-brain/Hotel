11c598d960819e74d2577c132e12ebcc6c877e4b
liza <lisaminakova@gmail.com>
2019-09-19 23:46:27 +0300

    Накидала черновой вариант для расширяемого списка

--- src/components/dropdown-list/dropdown-list.js
+++ src/components/dropdown-list/dropdown-list.js
@@ -1,4 +1,9 @@
+import "../checkbox/checkbox";
 import "./dropdown-list.scss";
+
 $(document)
-  .find("#some-id")
-  .on("click", console.log("click"));
+  .find(".some-select")
+  .on("click", function(e) {
+    console.log("click");
+    $(".some-options").toggleClass("some-options_open");
+  });
