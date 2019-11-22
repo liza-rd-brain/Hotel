692401dba001752b0c4d11d96e163df4209111d2
liza <lisaminakova@gmail.com>
2019-11-22 20:12:48 +0300

    Начала писать свою стилизацию и функционал doughnut-chart

--- src/components/doughnut-chart/doughnut-chart.js
+++ src/components/doughnut-chart/doughnut-chart.js
@@ -1,86 +1,154 @@
-/* import ChartJS from "chartjs-plugin-labels"; */
-/* import "chart.js"; */
-/* var ctx = document.getElementById("myChart2").getContext("2d"); */
-/* 
-new Chart(document.getElementById("myChart2"), {
-  type: "doughnut",
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "green";
+ctx.beginPath();
+ctx.arc(50, 50, 20, 0, Math.PI, false);
+ctx.stroke();
+
+var gradient_1 = ctx.createLinearGradient(0, 0, 0, 180);
+gradient_1.addColorStop(0, "#FFE39C");
+gradient_1.addColorStop(1, "#ffba9c");
+var gradient_2 = ctx.createLinearGradient(0, 180, 0, 0);
+gradient_2.addColorStop(0, "#6FCF97");
+gradient_2.addColorStop(1, "#66D2EA");
+var gradient_3 = ctx.createLinearGradient(0, 0, 0, 180);
+gradient_3.addColorStop(0, "#BC9CFF");
+gradient_3.addColorStop(1, "#8BA4F9");
+
+class Doughnut {
+  constructor(options) {
+    this.options = options;
+    this.canvas = options.canvas;
+    this.ctx = this.canvas.getContext("2d");
+    this.colors = options.colors;
+    this.drawPieSlice = this.drawPieSlice.bind(this);
+    this.drawLegendLabel = this.drawLegendLabel.bind(this);
+  }
+
+  drawPieSlice(ctx, centerX, centerY, radius, startAngle, endAngle, color) {
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.moveTo(centerX, centerY);
+    ctx.arc(centerX, centerY, radius, startAngle, endAngle);
+    ctx.closePath();
+    ctx.fill();
+  }
+
+  drawLegendLabel(ctx, centerX, centerY, radius, color) {
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    /*    ctx.moveTo(centerX, centerY); */
+    ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+    /*     ctx.closePath(); */
+    ctx.fill();
+  }
+
+  drawInnerText() {}
+
+  drawBorder() {}
+
+  draw() {
+    let total_value = 0;
+    let color_index = 0;
+    //вычисляем общее значание value
+    //переписать редьюсом
+    for (var item in this.options.data) {
+      var val = this.options.data[item];
+      total_value += val;
+    }
+    //вычисляем угол среза
+    //параметризовать по радианам
+    var start_angle = 4.71;
+    /*  var start_angle = 2 * Math.PI * val; */
+    for (var item in this.options.data) {
+      val = this.options.data[item];
+      var slice_angle = (2 * Math.PI * val) / total_value;
+
+      //параметрирозвать ширина и вырезанный круг
+      this.drawPieSlice(
+        this.ctx,
+        60,
+        60,
+        60,
+        start_angle,
+        start_angle + slice_angle,
+        this.colors[color_index]
+      );
+
+      start_angle += slice_angle;
+      color_index++;
+    }
+
+    if (this.options.doughnutHoleSize) {
+      this.drawPieSlice(this.ctx, 60, 60, 56, 0, 2 * Math.PI, "white");
+    }
+
+    //легенда
+    //проблема-не возьмет градиент от канвы, поэтому нужно будет переделать легенду на канву, а не HTML
+    if (this.options.legend) {
+      let color_index = 0;
+      let y = 40;
+      for (let item in this.options.data) {
+        this.drawLegendLabel(
+          this.ctx,
+          130,
+          y,
+          10,
+
+          this.colors[color_index]
+        );
+        color_index++;
+        /* y+=20 */
+        //нарисовали текст-вынести в функцию
+        ctx.font = "12px serif";
+        ctx.fillStyle = "black";
+        ctx.fillText(item, 150, y, 320);
+        y += 20;
+
+        /*  legendHTML +=
+          "<div><span style='display:inline-block;width:20px;background-color:" +
+          this.colors[color_index++] +
+          ";'>&nbsp;</span> " +
+          item +
+          "</div>"; */
+      }
+      /*  this.options.legend.innerHTML = legendHTML; */
+    }
+  }
+}
+
+/* var myLegend = document.getElementById("myLegend"); */
+var myDougnutChart = new Doughnut({
+  canvas: canvas,
   data: {
-    labels: [
-      "ONE",
-      "TWO",
-      "THREE",
-      "FOUR",
-      "FIVE",
-      "SIX",
-      "SEVEN",
-      "EIGHT",
-      "NINE",
-      "TEN"
-    ],
+    Хорошо: 25,
+    Удовлетворительно: 25,
+    Великолепно: 50,
+    Разочарован: 0
+  },
+  colors: [gradient_3, gradient_2, gradient_1],
+  doughnutHoleSize: 0.8,
+  legend: true
+});
+myDougnutChart.draw();
+
+/* var myDoughnut = new Doughnut(canvas, {
+  data: {
+    labels: ["Хорошо", "Удовлетворительно", "Великолепно", "Разочарован"],
     datasets: [
       {
-        backgroundColor: [
-          "#FF3784",
-          "#36A2EB",
-          "#4BC0C0",
-          "#F77825",
-          "#9966FF",
-          "#00A8C6",
-          "#379F7A",
-          "#CC2738",
-          "#8B628A",
-          "#8FBE00"
-        ],
-        data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        data: [25, 25, 50, 0],
+        backgroundColor: [gradient_3, gradient_2, gradient, "black"],
+        hoverBorderColor: [gradient_3, gradient_2, gradient],
+        hoverBorderWidth: [10, 10, 10],
+        hoverRadius: 10,
+        borderWidth: [1, 1, 1],
+        borderAlign: "inner",
+        shadowOffsetX: 3,
+        shadowOffsetY: 3,
+        shadowBlur: 10
       }
     ]
-  },
-  options: {
-    animation: {
-      animateRotate: true
-    },
-    events: ["click"],
-    layout: {
-      padding: {
-        left: 0,
-        right: 0,
-        top: 0,
-        bottom: 0
-      }
-    },
-    legend: {
-      display: false
-    },
-    title: {
-      display: true
-    },
-    tooltips: {
-      mode: "index"
-    },
-    elements: {
-      customCutout: true
-    },
-    cutoutPercentage: 92
   }
 }); */
-
-/*   // custom render
-  {
-    render: function (args) {
-      // args will be something like:
-      // { label: 'Label', value: 123, percentage: 50, index: 0, dataset: {...} }
-      return '$' + args.value;
-  
-      // return object if it is image
-      // return { src: 'image.png', width: 16, height: 16 };
-    }
-  }
-  
-  // dynamic fontColor
-  {
-    fontColor: function (args) {
-      // args will be something like:
-      // { index: 0, dataset: {...} }
-      return myColorTransfer(args.dataset.backgroundColor[index]);
-    }
-  } */
--- src/components/pie-chart/pie-chart.js
+++ src/components/pie-chart/pie-chart.js
@@ -1,10 +1,17 @@
 import "./pie-chart.scss";
-import Charts from "chart.js";
+import Chart from "chart.js";
+import "chartjs-plugin-style";
 /* var myChart = new Chart(ctx, {...}); */
 
-var canvas = document.getElementById("myCharts");
+var canvas = document.getElementById("myChart");
 var ctx = canvas.getContext("2d");
 var innerText = "привет";
+
+var effectColors = {
+  highlight: "rgba(255, 255, 255, 0.75)",
+  shadow: "rgba(0, 0, 0, 0.5)",
+  glow: "rgb(255, 255, 225)"
+};
 
 var gradient = ctx.createLinearGradient(0, 0, 0, 360);
 gradient.addColorStop(0, "#FFE39C");
@@ -19,23 +26,36 @@ gradient_3.addColorStop(1, "#8BA4F9");
 //сэмулировать с самого начала наведение на элемент удовлетворительно
 //убрать unhover c элементов
 //посмотреть плагин, который центрирует надпись
-var myCharts = new Charts(canvas, {
+var myChart = new Chart(canvas, {
   type: "doughnut",
   data: {
     labels: ["Хорошо", "Удовлетворительно", "Великолепно", "Разочарован"],
     datasets: [
       {
-        /* label: "# of Votes", */
+        /*  label: "# of Votes", */
         data: [25, 25, 50, 0],
-        /* borderWidth: 20, */
-       /*  radius: 1, */
-        //black поменять на градиент
         backgroundColor: [gradient_3, gradient_2, gradient, "black"],
-        // hoverBorderColor: [gradient_3, gradient_2, gradient],
-        hoverBorderWidth: [50,50,50],
-        borderWidth:[1,1,1],
+        hoverBorderColor: [gradient_3, gradient_2, gradient],
+        hoverBorderWidth: [10, 10, 10],
+
+        /*  hoverRadius: 10,
+        hitRadius: 10, */
+        borderWidth: [1, 1, 1],
+
         //не получается добиться нужно ширины
-        borderAlign: "inner"
+        borderAlign: "inner",
+        shadowOffsetX: 3,
+        shadowOffsetY: 3,
+        shadowBlur: 10,
+      /*   shadowColor: effectColors.shadow, */
+        bevelWidth: 2,
+        /* bevelHighlightColor: effectColors.highlight, */
+       /*  bevelShadowColor: effectColors.shadow, */
+        /* hoverInnerGlowWidth: 20,
+        hoverInnerGlowColor: effectColors.glow, */
+        hoverOuterGlowWidth:10,
+        hoverOuterGlowColor: effectColors.glow
+        /*      data: [6, 3, 2, 0] */
       }
     ]
   },
@@ -62,8 +82,8 @@ var myCharts = new Charts(canvas, {
         boxWidth: 12,
         usePointStyle: true
         /*    style: "point" */
-      },
-     /*  generateLabels: {
+      }
+      /*  generateLabels: {
         pointStyle: "circle"
       } */
     },
@@ -83,8 +103,8 @@ var myCharts = new Charts(canvas, {
     {
       id: "my-plugin",
       afterDraw: function(chart, option) {
-        var innerText = myCharts.active
-          ? myCharts.active[0]._model.label
+        var innerText = myChart.active
+          ? myChart.active[0]._model.label
           : "test";
 
         chart.ctx.fillStyle = "black";
@@ -93,8 +113,8 @@ var myCharts = new Charts(canvas, {
         chart.ctx.font = "17px Arial";
         chart.ctx.fillText(
           innerText,
-          /*  myChart.tooltip._lastActive,
-          myChart.active[0]._model.label || "test", */
+          //  myChart.tooltip._lastActive,
+          // myChart.active[0]._model.label || "test",
           chart.canvas.width / 2,
           chart.canvas.height / 2
         );
@@ -102,7 +122,3 @@ var myCharts = new Charts(canvas, {
     }
   ]
 });
-
-/* document.getElementById("chartjsLegend").innerHTML = myCharts.generateLegend();
- */
-/* document.getElementById(".myCharts").onclick = function(evt) {}; */
